037483a464547d4802455c0278fd61e512c545cc
Jan Kristoffer Cheng <Jan Kristoffer Cheng>
2017-02-25 17:22:07 +0800

    Change to postgres kasi pwet ng mysql

--- ThesisData/settings.py
+++ ThesisData/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import environ
-import pymysql
-pymysql.install_as_MySQLdb()
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
