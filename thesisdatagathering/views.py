89a6f59ab4553c74b519627528fa9e9e0cbf7f9c
Jan Kristoffer Cheng <Jan Kristoffer Cheng>
2017-03-16 16:38:37 +0800

    Changed something in requirements

--- thesisdatagathering/views.py
+++ thesisdatagathering/views.py
@@ -217,7 +217,7 @@ def thanks(request, redirect_url='/?sent=true'):
         date = tweet['created_at']
 
         text = html.unescape(text)
-        
+
         date_object = parse(date)
         delta = datetime.datetime.now().replace(tzinfo=None) - date_object.replace(tzinfo=None)
 
